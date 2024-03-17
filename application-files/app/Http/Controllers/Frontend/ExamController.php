55d0274f754bac0b906748eb547694919d18aa22
rabigorkhali <rabigorkhali@macbookairs-MacBook-Pro.local>
2024-03-17 23:42:31 +0545

    chore: exam form for student partially added

--- application-files/app/Http/Controllers/Frontend/ExamController.php
+++ application-files/app/Http/Controllers/Frontend/ExamController.php
@@ -3,16 +3,27 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Model\Student;
+use App\Repositories\System\QuestionnaireRepository;
 use Throwable;
 
 class ExamController extends Controller
 {
+    public function __construct(QuestionnaireRepository $questionnaireRepository, Student $studentModel)
+    {
+        $this->questionnaireRepository = $questionnaireRepository;
+        $this->studentModel = $studentModel;
+    }
+
     public function showExamForm($questionnaireId, $studentId)
     {
         try {
-            return view('frontend.examForm');
+            $data['questionnaires'] = $this->questionnaireRepository->query()->wheredate('expiry_date', '>', date('Y-m-d'))->find($questionnaireId);
+            $data['student'] = $this->studentModel->find($studentId);
+            return view('frontend.examForm', $data);
         } catch (Throwable $th) {
             dd($th->getMessage());
+            return redirect()->route('error.page.frontend')->withErrors(['alert-danger' => 'Something went wrong. Please contact support team.']);
         }
     }
 }
--- application-files/resources/views/frontend/examForm.blade.php
+++ application-files/resources/views/frontend/examForm.blade.php
@@ -1,4 +1,66 @@
 @extends('frontend.main')
 @section('form')
-I am here
+    <form action="">
+        @csrf
+        <div class="modal-body message-body">
+
+            <div class="container">
+                <h1 class="text-center">{{ $questionnaires->title }} </h1>
+
+                <label>Student Name: {{ $student->name }} </label><br>
+                <label>Student Email: {{ $student->email }} </label>
+                <label class="pull-right">Expiry Date: {{ $questionnaires->expiry_date }} </label>
+                <h3 class="text-center mt-4">Physics Question</h3>
+                @foreach ($questionnaires->physicsQuestions as $questionsKey => $questionData)
+                    <div class="card" style="margin-bottom: 0px">
+                        <div class="card-body">
+                            <p class="card-text">
+                                {{ listFormatting(SN(pageIndex($questionData), $questionsKey)) . $questionData->question }}
+                            </p>
+                            <ul class="list-group list-group-flush">
+                                @php
+                                    $optionIncrement = 'a';
+                                @endphp
+                                @foreach (json_decode($questionData->options, true) as $questionOptionKey => $questionOptionData)
+                                    <li class="list-group-item">
+                                        <input type="radio" name="{{$questionsKey}}Physics"> {{listFormatting($optionIncrement) . $questionOptionData[$optionIncrement] }}
+                                    </li>
+                                    @php
+                                        $optionIncrement = chr(ord($optionIncrement) + 1);
+                                    @endphp
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+        <div class="modal-body message-body">
+            <div class="container">
+                <h3 class="text-center">Chemistry Question</h3>
+                @foreach ($questionnaires->chemistryQuestions as $questionsKey => $questionData)
+                    <div class="card" style="margin-bottom: 0px">
+                        <div class="card-body">
+                            <p class="card-text">
+                                {{ listFormatting(SN(pageIndex($questionData), $questionsKey)) . $questionData->question }}
+                            </p>
+                            <ul class="list-group list-group-flush">
+                                @php
+                                    $optionIncrement = 'a';
+                                @endphp
+                                @foreach (json_decode($questionData->options, true) as $questionOptionJey => $questionOptionData)
+                                    <li class="list-group-item">
+                                        <input type="radio" name="{{$questionsKey}}Chemistry">   {{ listFormatting($optionIncrement) . $questionOptionData[$optionIncrement] }}
+                                    </li>
+                                    @php
+                                        $optionIncrement = chr(ord($optionIncrement) + 1);
+                                    @endphp
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </form>
 @endsection
